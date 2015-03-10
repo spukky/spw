4bc483896fa343947e2b1c39d64d9e623a663cd4
spukky <darknessknight.darkness@gmail.com>
2015-03-10 21:42:08 +0700

    Update Main.java

--- f2/spw/Main.java
+++ f2/spw/Main.java
@@ -8,10 +8,10 @@ public class Main {
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Space War");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400, 650);
+		frame.setSize(500, 850);
 		frame.getContentPane().setLayout(new BorderLayout());
 		
-		SpaceShip v = new SpaceShip(180, 550, 20, 20);
+		SpaceShip v = new SpaceShip(150, 450, 20, 20);
 		GamePanel gp = new GamePanel();
 		GameEngine engine = new GameEngine(gp, v);
 		frame.addKeyListener(engine);
